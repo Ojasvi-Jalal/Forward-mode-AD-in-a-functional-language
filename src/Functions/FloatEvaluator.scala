7e262a0cec05da1ac047ce505d7a8a00a4555590
Ojasvi-Jalal <s1612970@sms.ed.ac.uk>
2019-10-29 20:14:28 +0000

    exponents

--- src/Functions/FloatEvaluator.scala
+++ src/Functions/FloatEvaluator.scala
@@ -1,6 +1,7 @@
 package Functions
 
 import scala.collection.mutable
+import scala.language.implicitConversions
 
 object FloatEvaluator {
   val paramToArg = mutable.HashMap[Param,Expr]()
@@ -11,15 +12,9 @@ object FloatEvaluator {
       case FunctionCall(FunctionCall(_:MultiplyFloat, arg2),arg1) => eval(arg1) * eval(arg2)
       case FunctionCall(FunctionCall(_:DivideFloat, arg2),arg1) => eval(arg1) / eval(arg2)
       case FunctionCall(FunctionCall(_:PowerFloat, arg2),arg1) => {
-        var result = 1.0
-        var i = 0.0
-        java.lang.Integer.valueOf(arg2)
-        if(arg2 == 0.0)
-          1
-        for(i <- 1 to ){
-          result =  result * eval(arg1)
-        }
-        result.toFloat
+        val exponent  = java.lang.Double.valueOf(eval(arg1))
+        val base      = java.lang.Double.valueOf(eval(arg2))
+        scala.math.pow(base,exponent).asInstanceOf[Float]
       }
       case FunctionCall(Lambda(param,body),arg) =>
         // store in a map   param -> arg and eval body
