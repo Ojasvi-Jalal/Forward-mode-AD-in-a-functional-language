29de9052fc675ca1e0cdd22d3b3b071f5db86cdb
Ojasvi-Jalal <s1612970@sms.ed.ac.uk>
2019-11-02 14:17:19 +0000

    added typechecking for all operations

--- src/test/Test1.scala
+++ src/test/Test1.scala
@@ -7,11 +7,10 @@ import typeCheck.TypeChecker
 object Test1 extends App{
     val var1 = DoubleLiteral(3)
     val var2 = DoubleLiteral(4)
-    val sum  =  var1 + var2 //ExprFunctions.+(var1,var2)
+    val sum  =  var1 + var2
     val product = var1 * var2
     val division = var1 / var2
     val power = var1 ^ var2
-    //println(s"sum of $var1 and $var2 = $sum")
     println("var1 = "+ Evaluator.eval(var1))
     println("var2 = "+ Evaluator.eval(var2))
     println("sum = "+Evaluator.eval(sum))
@@ -22,6 +21,8 @@ object Test1 extends App{
     println(TypeChecker.typeCheck(var2))
     println(TypeChecker.typeCheck(sum))
     println(TypeChecker.typeCheck(product))
+    println(TypeChecker.typeCheck(division))
+    println(TypeChecker.typeCheck(power))
 
 
 }
--- src/typeCheck/FloatTypeChecker.scala
+++ src/typeCheck/FloatTypeChecker.scala
@@ -1,22 +1,27 @@
 package typeCheck
 
-import intermediateRep.{Expr, Type, DoubleLiteral, FunctionCall, AddDouble, MultiplyDouble}
+import intermediateRep.{Expr, Type, DoubleLiteral, FunctionCall, AddDouble, MultiplyDouble, DivideDouble, PowerDouble}
 
 object FloatTypeChecker {
-  def typeCheck(e:Expr) : Type = {
+  def typeCheck(e: Expr): Type = {
     e match {
       case DoubleLiteral(e) => DoubleLiteral(e).t
-      case FunctionCall(FunctionCall(_:AddDouble, arg2),arg1) => {
-        val typeArg1 = typeCheck(arg1)
-        val typeArg2 = typeCheck(arg2)
-//        println(typeCheck(arg1))
-//        println(typeCheck(arg1))
-//        val isSameType =  (typeArg1 == typeArg2)
-//        println(isSameType)
-        return typeCheck(arg1)}
-      case FunctionCall(FunctionCall(_:MultiplyDouble, arg2),arg1) => {
+      case FunctionCall(FunctionCall(_: AddDouble, arg2), arg1) => {
         assert(typeCheck(arg1) === typeCheck(arg2))
-        return typeCheck(arg1)}
+        return typeCheck(arg1)
+      }
+      case FunctionCall(FunctionCall(_: MultiplyDouble, arg2), arg1) => {
+        assert(typeCheck(arg1) === typeCheck(arg2))
+        return typeCheck(arg1)
+      }
+      case FunctionCall(FunctionCall(_: DivideDouble, arg2), arg1) => {
+        assert(typeCheck(arg1) === typeCheck(arg2))
+        return typeCheck(arg1)
+      }
+      case FunctionCall(FunctionCall(_: PowerDouble, arg2), arg1) => {
+        assert(typeCheck(arg1) === typeCheck(arg2))
+        return typeCheck(arg1)
       }
     }
   }
+}
